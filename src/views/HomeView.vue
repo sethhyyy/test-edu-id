632a9987a38b818994ef05766c96f8879acf9138
Setthaphong Songjaroen <seth.setthaphong@kkumail.com>
2026-06-25 16:29:09 +0700

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>Test Edu ID</h1>
     <v-btn @click="callAPI">Test Edu ID</v-btn>
+    <v-btn @click="healthCheck">Health Check</v-btn>
   </div>
 </template>
 <script>
@@ -10,7 +11,6 @@
     name: 'HomePage',
     mounted() {
       axios.get(`${process.env.VUE_APP_API_URL}/api/hello`)
-      // fetch('https://test-edu-id-be-dz8s.vercel.app/api/hello')
         .then(res => res.json())
         .then(data => {
           this.message = data.message
@@ -18,9 +18,21 @@
         .catch(err => console.error(err))
     },
     methods: {
-      callAPI () {
-        axios.get(`${process.env.VUE_APP_API_URL}/api/hello`)
-        // fetch('https://test-edu-id-be-dz8s.vercel.app/api/hello')
+      async callAPI () {
+        const client_id = ''
+        const redirect_uri = ''
+        const response_type = 'code'
+        const scope = 'person_id passport_number prefix first_name last_name birth_date gender_id'
+        await axios({
+          method: 'GET',
+          url: `${process.env.EDU_ID_URL}/oauth2/v1/authorize?client_id${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+      },
+      async healthCheck () {
+      axios.get(`${process.env.VUE_APP_API_URL}/api/health`)
         .then(res => res.json())
         .then(data => {
           this.message = data.message
