7fa3e943b1996c7ccf97dfb97e92490653bcf735
Setthaphong Songjaroen <seth.setthaphong@kkumail.com>
2026-06-25 16:59:44 +0700

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,7 +26,7 @@
         const scope = 'person_id passport_number prefix first_name last_name birth_date gender_id'
         await axios({
           method: 'GET',
-          url: `https://uat-edu.id.th/oauth2/v1/authorize?client_id${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`,
+          url: `https://uat-edu.id.th/oauth2/v1/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`,
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
           }
