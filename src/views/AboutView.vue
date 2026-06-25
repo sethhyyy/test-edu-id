ab799b01993d34950ea200650591f71f3c4f8bd8
Setthaphong Songjaroen <seth.setthaphong@kkumail.com>
2026-06-25 14:19:10 +0700

    ADD | Real First Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+  export default {
+    mounted() {
+      fetch('/api/hello')
+        .then(res => res.json())
+        .then(data => {
+          this.message = data.message
+        })
+        .catch(err => console.error(err))
+    }
+  }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>Test Edu ID</h1>
+    <v-btn @click="callAPI">Test Edu ID</v-btn>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'HomePage',
+    mounted() {
+      fetch('/api/hello')
+        .then(res => res.json())
+        .then(data => {
+          this.message = data.message
+        })
+        .catch(err => console.error(err))
+    },
+    methods: {
+      callAPI () {
+        fetch('/api/hello')
+        .then(res => res.json())
+        .then(data => {
+          this.message = data.message
+        })
+        .catch(err => console.error(err))
+      }
+    }
+  }
+</script>
